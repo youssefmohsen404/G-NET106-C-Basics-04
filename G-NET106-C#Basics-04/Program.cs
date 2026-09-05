b86cd2a8a228df6bf5712731d18c11b74b2eeeec
youss <youssmohss111@gmail.com>
2026-09-06 02:14:52 +0300

    Add project files.

---
+++ G-NET106-C#Basics-04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET106_C_Basics_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
