cca7a04827fb1b631c7e0eeaa90476dddaebf381
youss <youssmohss111@gmail.com>
2026-09-06 02:28:45 +0300

    Q2

--- G-NET106-C#Basics-04/Program.cs
+++ G-NET106-C#Basics-04/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace G_NET106_C_Basics_04
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace G_NET106_C_Basics_04
 {
     internal class Program
     {
@@ -11,7 +13,16 @@
             /*double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine(prices[1]);*/
             #endregion
-
+            #region Question 2
+            //Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and 
+            //shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+           /* int[,] shelfCopies = new int[2, 2]
+            {
+                {3,5 },
+                {1,4 }
+            };
+            Console.WriteLine(shelfCopies[1,0]);*/
+            #endregion
         }
     }
 }
