50816aa0d8beb485cd1af7b8ca951638ddea047a
youss <youssmohss111@gmail.com>
2026-09-06 02:21:20 +0300

    Q1

--- G-NET106-C#Basics-04/Program.cs
+++ G-NET106-C#Basics-04/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1 
+            //Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print 
+            /*the second price(index 1).*/
+
+            /*double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);*/
+            #endregion
+
         }
     }
 }
